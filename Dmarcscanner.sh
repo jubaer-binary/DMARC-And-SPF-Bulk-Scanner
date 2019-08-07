daeaffb352b93c82a5c1a414c7d6569f0edef14b
Jubaer Alnazi <51982912+jubaer-binary@users.noreply.github.com>
2019-08-07 13:26:05 +0800

    Create Dmarcscanner.sh

--- Dmarcscanner.sh
+++ Dmarcscanner.sh
@@ -3,13 +3,14 @@
 RED=$(echo -en '\001\033[00;31m\002')
 GREEN=$(echo -en '\001\033[00;32m\002')
 WHITE=$(echo -en '\001\033[01;37m\002')
-
+​
 echo "${WHITE}Enter File Location : "
 read location
+echo "##########################-----------------DMARC And SPF SCANNER by Jubaer Alnazi---------------------###############################"
 input="$location"
 while IFS= read -r line
 do
-execute1=$(dig TXT _dmarc.$line | grep 'v=DMARC\|SOA')
-
-echo -e "${GREEN}$execute1"
+execute1=$(dig TXT _dmarc.$line | GREP_COLORS='mt=01;32' egrep --color=always 'v=DMARC|\SOA')
+spf=$(nslookup -type=txt $line | GREP_COLORS='mt=01;32' egrep --color=always 'v=spf')
+echo -e "${GREEN}------------------------------------------------------------------------------------\n${GREEN}$execute1\n${WHITE}$spf"
 done < "$input"
