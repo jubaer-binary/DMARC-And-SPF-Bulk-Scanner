09e8a9e9faa3d9c706ba455e04d05ba8f20f391f
Jubaer Alnazi <51982912+jubaer-binary@users.noreply.github.com>
2019-08-07 12:42:06 +0800

    Create Dmarcscanner.sh

--- Dmarcscanner.sh
+++ Dmarcscanner.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-echo "Enter File Location : "
+#Colors
+RED=$(echo -en '\001\033[00;31m\002')
+GREEN=$(echo -en '\001\033[00;32m\002')
+WHITE=$(echo -en '\001\033[01;37m\002')
+
+echo "${WHITE}Enter File Location : "
 read location
 input="$location"
 while IFS= read -r line
 do
-echo "$(dig TXT _dmarc.$line | grep 'v=DMARC\|SOA');"
+execute1=$(dig TXT _dmarc.$line | grep 'v=DMARC\|SOA')
+
+echo -e "${GREEN}$execute1"
 done < "$input"
